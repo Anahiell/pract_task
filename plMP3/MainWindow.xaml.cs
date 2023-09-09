19aa0fcc09dba35ecf44f8393ad4a4ec35a897dd
Бурдейный Евгений <anahielitl@gmail.com>
2023-09-09 11:28:33 +0300

    media

---
+++ plMP3/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace plMP3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void choese_but_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Media Files|*.avi;*.mp4;*.mkv;*.mp3|All Files|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string selectedFilePath = openFileDialog.FileName;
+
+                string fileExtension = System.IO.Path.GetExtension(selectedFilePath);
+
+                if (fileExtension.Equals(".mp3", StringComparison.OrdinalIgnoreCase))
+                {
+                    mediaElement1.Source = new Uri(selectedFilePath);
+                    mediaElement1.Play(); 
+                }
+                else
+                {
+                    mediaElement1.Source = new Uri(selectedFilePath);
+                }
+            }
+        }
+
+        private void play_but_Click(object sender, RoutedEventArgs e)
+        {
+            if (mediaElement1.Source != null)
+            {
+                mediaElement1.Play(); 
+            }
+        }
+
+        private void pause_but_Click(object sender, RoutedEventArgs e)
+        {
+            if (mediaElement1.Source != null)
+            {
+                mediaElement1.Pause();
+            }
+        }
+    }
+
+}
